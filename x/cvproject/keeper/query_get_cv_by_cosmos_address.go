059ca22661490d43e2cb7ace4694e454c615b31b
Alexey <aleksej.serbinov29@gmail.com>
2023-09-06 22:34:51 +0300

    new query file

---
+++ x/cvproject/keeper/query_get_cv_by_cosmos_address.go
@@ -0,0 +1,11 @@
+package keeper
+
+import (
+	"context"
+
+	"github.com/AlexS778/CVProject/x/cvproject/types"
+)
+
+func (k Keeper) CvByCosmosAddress(context.Context, *types.QueryCvByCosmosAddressRequest) (*types.QueryCvByCosmosAddressResponse, error) {
+	panic("unimplemented")
+}
